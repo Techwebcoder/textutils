a8baad6e6f5bddbfdd19298641725ac57b33fea8
Techwebcoder <rajakumar.xia.11117@gmail.com>
2025-01-15 15:50:08 +0530

    Adding the files and this is the finished product website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import Navbar from './components/Navbar.js';
-import TextForm from './components/TextForm.js';
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import TextForm from "./components/TextForm.js";
 
 function App() {
   return (
     <>
-      <Navbar title = "TextUtils" abouttext = "About Us"/>
+      <Navbar title="TextUtils" abouttext="About Us" />
       <div className="container">
-      <TextForm heading = "Enter the text"/>
+        <TextForm heading="Enter the text" />
       </div>
     </>
   );
 }
 export default App;
-
-
