2726dacde964468d48a6a8bc1624d546b18fe579
techwebcoder <rajakumar.xia.11117@gmail.com>
2025-01-18 20:15:05 +0530

    dark mode is done in this project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import Navbar from "./components/Navbar.js";
 import TextForm from "./components/TextForm.js";
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode ,setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" abouttext="About Us" />
+      <Navbar title="TextUtils" abouttext="About Us" toggleMode = {toggleMode} mode = {mode} />
       <div className="container">
-        <TextForm heading="Enter the text" />
+        <TextForm heading="Enter the text" mode = {mode} />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,6 +13,15 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleExtra = () => {
+    let  newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
   const handleCapclick = () => {
     let newText = text
       .split(' ')
@@ -37,31 +46,38 @@ export default function TextForm(props) {
     <>
       <div>
         <div className="mb-3">
-          <h1 className="my-5 text-center">{props.heading}</h1>
+          <h1 className="my-5 text-center"  style ={{color : props.mode==='dark'?'white':'#042743'}}>{props.heading}</h1>
           <textarea
             className="form-control mb-3"
             id="myBox"
+            style ={{backgroundColor : props.mode==='light'?'white':'grey' ,color : props.mode==='light'?'#042743':'white'}}
             value={text}
             onChange={handleOnchange}
             rows="8"
           ></textarea>
           <div className="d-flex justify-content-center">
-            <button type="button" className="btn btn-primary mx-5 my-3" onClick={handleUpclick}>
+            <button type="button" className="btn btn-primary mx-4 my-3" onClick={handleUpclick}>
               Change to Uppercase
             </button>
-            <button type="button" className="btn btn-warning mx-5 my-3" onClick={handleLoclick}>
+            <button type="button" className="btn btn-warning mx-4 my-3" onClick={handleLoclick}>
               Change to Lowercase
             </button>
-            <button type="button" className="btn btn-dark mx-5 my-3" onClick={handleCapclick}>
+            <button type="button" className="btn btn-dark mx-4 my-3" onClick={handleCapclick}>
               Change to Capitalize
             </button>
-            <button type="button" className="btn btn-danger mx-5 my-3" onClick={handleClearclick}>
+            <button type="button" className="btn btn-secondary mx-4 my-3" onClick={handleExtra}>
+              Remove Extraspace 
+            </button>
+            <button type="button" className="btn btn-info mx-4 my-3" onClick={handleCopy}>
+              Copy Text
+            </button>
+            <button type="button" className="btn btn-danger mx-4 my-3" onClick={handleClearclick}>
               Clear Text
             </button>
           </div>
         </div>
       </div>
-      <div className="container">
+      <div className="container" style ={{color : props.mode==='dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
         <p>{getWordCount()} Words and {text.length} Characters</p>
         <p>{(0.008 * getWordCount()).toFixed(3)} Minutes read</p>
